4a6464f32ee4ecfb65cfd95daa81c4b413dfb8f8
Rupinder Dhariwal <rd@truss.works>
2021-03-15 14:01:35 -0400

    add policy for registering new ECR images + update variable naming

--- main.tf
+++ main.tf
@@ -23,11 +23,45 @@ resource "aws_ecr_lifecycle_policy" "main" {
 # attach a ECR policy to a repository and give read only cross account access to external principal accounts
 resource "aws_ecr_repository_policy" "main" {
   repository = aws_ecr_repository.main.name
-  policy     = data.aws_iam_policy_document.ecr_read_perms.json
+  policy     = data.aws_iam_policy_document.ecr_perms_ro_cross_account.json
   count      = length(var.allowed_read_principals) > 0 ? 1 : 0
 }
 
-data "aws_iam_policy_document" "ecr_read_perms" {
+resource "aws_iam_policy" "main" {
+  name        = "githubactions-ecr-${var.container_name}-policy"
+  description = "Allow githubActions to push new inspec-profile-aws-mod ECR images"
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Sid      = ""
+        Action   = "ecr:GetAuthorizationToken"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Sid      = ""
+        Action   = [
+          "ecr:UploadLayerPart",
+          "ecr:PutImage",
+          "ecr:ListImages",
+          "ecr:InitiateLayerUpload",
+          "ecr:GetRepositoryPolicy",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:DescribeRepositories", "ecr:DescribeImages",
+          "ecr:CompleteLayerUpload",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+        ]
+        Effect   = "Allow"
+        Resource = aws_ecr_repository.main.arn
+      }
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "ecr_perms_ro_cross_account" {
 
   statement {
     sid = "CrossAccountReadOnly"
