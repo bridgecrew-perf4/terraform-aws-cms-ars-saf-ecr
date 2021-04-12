725ddbfb65e58f24b3a47c272dae0a0c698faef7
Rupinder Dhariwal <rd@truss.works>
2021-04-12 15:32:16 -0400

    pass pre-commit checks, fixed one globbing issues and one env variable used but not defined linting err

--- scriptRunner.sh
+++ scriptRunner.sh
@@ -3,8 +3,9 @@
 set -eo pipefail
 
 # if the s3_bucket_path is not provided, do not push json file to the bucket
+# shellcheck disable=SC2154
 if [[ -n $s3_bucket_path ]]; then
-    inspec exec ./cms-ars-3.1-moderate-aws-foundations-cis-overlay --reporter json | aws s3 cp - $s3_bucket_path
+    inspec exec ./cms-ars-3.1-moderate-aws-foundations-cis-overlay --reporter json | aws s3 cp - "$s3_bucket_path"
 fi
 
 #
