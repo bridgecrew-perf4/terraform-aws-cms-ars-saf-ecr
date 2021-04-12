2517e91ca6077aa998b179c1c7084e72e3c6e77b
Rupinder Dhariwal <rd@truss.works>
2021-04-12 16:46:34 -0400

    fix path to profile

--- scriptRunner.sh
+++ scriptRunner.sh
@@ -5,8 +5,8 @@ set -eo pipefail
 # if the s3_bucket_path is not provided, do not push json file to the bucket
 # shellcheck disable=SC2154
 if [[ -n $s3_bucket_path ]]; then
-    inspec exec ./cms-ars-3.1-moderate-aws-foundations-cis-overlay --reporter json --target aws:// --chef-license accept-silent | aws s3 cp - "$s3_bucket_path"
+    inspec exec /cms-ars-3.1-moderate-aws-foundations-cis-overlay --reporter json --target aws:// --chef-license accept-silent | aws s3 cp - "$s3_bucket_path"
 fi
 
 #
-inspec exec ./cms-ars-3.1-moderate-aws-foundations-cis-overlay --target aws:// --chef-license accept-silent --no-color
+inspec exec /cms-ars-3.1-moderate-aws-foundations-cis-overlay --target aws:// --chef-license accept-silent --no-color
