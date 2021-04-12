a04370ea55f890435c879cc74157dc9af9364d9a
Rupinder Dhariwal <rd@truss.works>
2021-04-12 15:27:07 -0400

    kill trailing spaces

--- scriptRunner.sh
+++ scriptRunner.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 # if the s3_bucket_path is not provided, do not push json file to the bucket
-if [[ -n $s3_bucket_path ]]; then 
+if [[ -n $s3_bucket_path ]]; then
     inspec exec ./cms-ars-3.1-moderate-aws-foundations-cis-overlay --reporter json | aws s3 cp - $s3_bucket_path
 fi
 
