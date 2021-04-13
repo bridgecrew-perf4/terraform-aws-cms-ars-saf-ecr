2e42e8fe35ecb3935a1b3528f19ef13b0765f605
Rupinder Dhariwal <rd@truss.works>
2021-04-13 11:05:05 -0400

    add tracers in code for debugging s3 bucket uploads

--- scriptRunner.sh
+++ scriptRunner.sh
@@ -8,5 +8,8 @@ inspec exec /home/default/profiles/cms-ars-3.1-moderate-aws-foundations-cis-over
 # if the s3_bucket_path is not provided, do not push json file to the bucket
 # shellcheck disable=SC2154
 if [[ -n $s3_bucket_path ]]; then
+    echo "s3_bucket_path values found: $s3_bucket_path"
     inspec exec /home/default/profiles/cms-ars-3.1-moderate-aws-foundations-cis-overlay --reporter json --target aws:// --chef-license accept-silent | aws s3 cp - "$s3_bucket_path"
+else
+    echo "s3_bucket_path variable not found, skipping s3 results upload."
 fi
