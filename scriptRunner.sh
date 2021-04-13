a2550665403ab046be005ce0b20a3b2d2c1f42d9
Rupinder Dhariwal <rd@truss.works>
2021-04-13 15:14:10 -0400

    remove the option -e from the shell command as the inspec tool uses non zero exit code and create premature exit

--- scriptRunner.sh
+++ scriptRunner.sh
@@ -1,20 +1,21 @@
 #! /usr/bin/env bash
 
-set -eo pipefail
+#  not using -e option as the inspec command returns non 0 error code
+set -o pipefail
 
 echo "starting Inpsec scan, start"
 
 # log output of inspec run to stdout
-#inspec exec /home/default/profiles/cms-ars-3.1-moderate-aws-foundations-cis-overlay --target aws:// --chef-license accept-silent --no-color
+inspec exec /home/default/profiles/cms-ars-3.1-moderate-aws-foundations-cis-overlay --target aws:// --chef-license accept-silent --no-color
 
 echo "starting Inpsec scan, stop"
-
 
 # if the s3_bucket_path is not provided, do not push json file to the bucket
 # shellcheck disable=SC2154
 if [[ -n $s3_bucket_path ]]; then
     echo "s3_bucket_path values found: $s3_bucket_path"
     inspec exec /home/default/profiles/cms-ars-3.1-moderate-aws-foundations-cis-overlay --reporter json --target aws:// --chef-license accept-silent | aws s3 cp - "$s3_bucket_path"
+    echo "complete"
 else
     echo "s3_bucket_path variable not found, skipping s3 results upload."
 fi
