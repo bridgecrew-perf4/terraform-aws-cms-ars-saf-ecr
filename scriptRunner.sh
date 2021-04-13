1bdf39605ca335045424fa82a184173f01cddc59
Rupinder Dhariwal <rd@truss.works>
2021-04-13 10:20:26 -0400

    reorder inspec execution statements to make the run more defenseive, s3 perm failures should not make a hard stop for the scanner logs

--- scriptRunner.sh
+++ scriptRunner.sh
@@ -2,10 +2,11 @@
 
 set -eo pipefail
 
+# log output of inspec run to stdout
+inspec exec /home/default/profiles/cms-ars-3.1-moderate-aws-foundations-cis-overlay --target aws:// --chef-license accept-silent --no-color
+
 # if the s3_bucket_path is not provided, do not push json file to the bucket
 # shellcheck disable=SC2154
 if [[ -n $s3_bucket_path ]]; then
     inspec exec /home/default/profiles/cms-ars-3.1-moderate-aws-foundations-cis-overlay --reporter json --target aws:// --chef-license accept-silent | aws s3 cp - "$s3_bucket_path"
 fi
-
-inspec exec /home/default/profiles/cms-ars-3.1-moderate-aws-foundations-cis-overlay --target aws:// --chef-license accept-silent --no-color
